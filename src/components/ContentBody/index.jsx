0f2be728cf95e674e3f68aac8b80bd3549f1276c
Marlon Monsores <marlonmonsores@live.com>
2023-09-24 22:26:44 -0300

    :lipstick: Create ContentBody component

---
+++ src/components/ContentBody/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ContentContainer } from './styles'
+
+export default function ContentBody() {
+  return (
+    <>
+        <ContentContainer>
+            <div>
+                <h3>
+                <span>MARLON </span>
+                MONSORES
+                </h3>
+                <p>Independent Web Developer</p>
+            </div>
+        </ContentContainer>
+    </>
+  )
+}
---
+++ src/components/ContentBody/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ContentContainer = styled.div`
+    h3 {
+        font-size: 55px;
+        line-height: 66px;
+        font-weight: 900;
+    }
+
+    h3 span {
+        color: transparent;
+        -webkit-text-stroke: 1px #000;
+    }
+
+    p { 
+        font-size: 17px;
+        font-weight: 500;
+        color: #767676;
+        line-height: 30px;
+        letter-spacing: .5px;
+    }
+
+`
