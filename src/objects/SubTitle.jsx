52de144b047ebe8061c127cdc518a15900744a87
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-09-29 08:19:51 -0300

    💄 create SubTitle object

---
+++ src/objects/SubTitle.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types'
+
+export const MainSubTitle = styled.p`
+  font-size: 17px;
+  font-weight: 500;
+  color: #767676;
+  line-height: 30px;
+  letter-spacing: .5px;
+`
+
+SubTitle.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default function SubTitle({ children }) {
+  return (
+    <MainSubTitle>{children}</MainSubTitle>
+  )
+}
