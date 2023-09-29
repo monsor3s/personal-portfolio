103c9cf7c25481532ec8d04e87a2ec525b1238b2
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-09-29 08:20:06 -0300

    💄 create Button object

---
+++ src/objects/Button.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types'
+
+export const MainButton = styled.button`
+  width: 206px;
+  height: 60px;
+  background-color: #000;
+  color: #fff;
+  padding: 13px 48px;
+  border: 2px solid #000;
+
+  &:hover {
+    background-color: transparent;
+    color: #000;
+    cursor: pointer;
+  }
+`
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default function Button({ children }) {
+  return (
+    <MainButton>{children}</MainButton>
+  )
+}
